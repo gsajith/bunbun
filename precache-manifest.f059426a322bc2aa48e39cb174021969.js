a3a817b6cab2bfd87729a9a3d2a7d4ce90654db9
Gautham Sajith <gsajith@umich.edu>
2019-09-30 16:19:07 -0400

    Updates

---
+++ precache-manifest.f059426a322bc2aa48e39cb174021969.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "417ea73a82fa772e79f7cb67a6e44a82",
+    "url": "/bunbun/index.html"
+  },
+  {
+    "revision": "9c35f7ff0689a91dcad7",
+    "url": "/bunbun/static/css/main.5dc064b7.chunk.css"
+  },
+  {
+    "revision": "3dc6dd1cded58f240c06",
+    "url": "/bunbun/static/js/2.4c6fda35.chunk.js"
+  },
+  {
+    "revision": "9c35f7ff0689a91dcad7",
+    "url": "/bunbun/static/js/main.ac55a2b6.chunk.js"
+  },
+  {
+    "revision": "a94f6f05cc64a4a2e079",
+    "url": "/bunbun/static/js/runtime-main.b7bbfd0a.js"
+  },
+  {
+    "revision": "d747335ac426f29fd58f999a4c1b6323",
+    "url": "/bunbun/static/media/LilitaOne-Regular.d747335a.ttf"
+  },
+  {
+    "revision": "42cf9f0820d16f3ac2c26a7710ce70f2",
+    "url": "/bunbun/static/media/Poppins-Black.42cf9f08.ttf"
+  },
+  {
+    "revision": "c23534acbeddbaadfd0ab2d2bbfdfc84",
+    "url": "/bunbun/static/media/Poppins-Bold.c23534ac.ttf"
+  },
+  {
+    "revision": "6b78c7ec468eb0e13c6c5c4c39203986",
+    "url": "/bunbun/static/media/Poppins-ExtraBold.6b78c7ec.ttf"
+  },
+  {
+    "revision": "8ea2657f263b6b2a404bfb57f4160013",
+    "url": "/bunbun/static/media/Poppins-ExtraLight.8ea2657f.ttf"
+  },
+  {
+    "revision": "2a47a29ceb33c966c8d79f8d5a5ea448",
+    "url": "/bunbun/static/media/Poppins-Light.2a47a29c.ttf"
+  },
+  {
+    "revision": "ba95810b56f476990ca71d15139d5111",
+    "url": "/bunbun/static/media/Poppins-Medium.ba95810b.ttf"
+  },
+  {
+    "revision": "41e8dead03fb979ecc23b8dfb0fef627",
+    "url": "/bunbun/static/media/Poppins-Regular.41e8dead.ttf"
+  },
+  {
+    "revision": "342ba3d8ac29ac8c38d7cef8efbf2dc9",
+    "url": "/bunbun/static/media/Poppins-SemiBold.342ba3d8.ttf"
+  },
+  {
+    "revision": "c0fafa8397437c95848724aed686d63b",
+    "url": "/bunbun/static/media/Poppins-Thin.c0fafa83.ttf"
+  },
+  {
+    "revision": "66347951b11c8194382f6055634d2c8c",
+    "url": "/bunbun/static/media/bakery.66347951.jpeg"
+  },
+  {
+    "revision": "69de3a180ff303bc15e1c71a28904830",
+    "url": "/bunbun/static/media/bunhome.69de3a18.jpeg"
+  }
+]);
